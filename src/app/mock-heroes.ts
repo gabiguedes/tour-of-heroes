04f225e4c2315d80f8b2ac9b0060fdcd1dc26fdb
Gabriela Guedes <zooiv3rde@gmail.com>
2022-03-13 10:09:49 -0300

    estudos - event binding

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dr Estranho' },
+  { id: 12, name: 'Flash' },
+  { id: 13, name: 'Batman' },
+  { id: 14, name: 'Capitão America' },
+  { id: 15, name: 'Magneto' },
+  { id: 16, name: 'Coringa' },
+  { id: 17, name: 'Jaspion' },
+  { id: 18, name: 'Pantera Negra' },
+  { id: 19, name: 'Arqueiro verde' },
+  { id: 20, name: 'Tornado' }
+];

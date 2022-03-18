1f74958a19818b08b88c9ce9296db22a15de11f6
Gabriela Guedes <zooiv3rde@gmail.com>
2022-03-18 10:39:18 -0300

    estudos - http options

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { catchError, Observable, of, tap } from 'rxjs';
 import { Hero } from './hero';
 import { MessageService } from './message.service';
 import { HEROES } from './mock-heroes';
@@ -17,9 +17,16 @@ export class HeroService {
     private messageService: MessageService
   ) { }
 
-  /** GET heroes from the server */
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => this.log('heróis buscados')),
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   }
 
   private log(message: string) {
@@ -27,9 +34,40 @@ export class HeroService {
   }
 
   getHero(id: number): Observable<Hero> {
-      const hero = HEROES.find(h => h.id === id)!; 
-      this.messageService.add(`HeroService: heróis buscados id=${id}`);
-      return of(hero);
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`buscando hero por id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+  
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+  
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+  
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
 }
